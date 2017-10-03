7d8319e4b2f98510149ad7e83d905b4f7983734f
Lucas Vieira <lucasvg@poli.ufrj.br>
2017-10-03 08:53:17 -0300

    Improve parameters

--- simulationRandomNumber.py
+++ simulationRandomNumber.py
@@ -1,15 +1,24 @@
 import sys
 
-# parameters of linear congruent method
+#### load parameters of Linear Congruent method
+# a of L. C. formula
 a = int(sys.argv[1])
+# M of L. C. formula
 m = int(sys.argv[2])
+# c of L. C. formula
 c = int(sys.argv[3])
+# seed of L. C. formula
 x0 = int(sys.argv[4])
+# amount of random numbers to generate
+n = int(sys.argv[5])
+#### end load parameters of linear congruent method
 
+numbers = []
 ##### generates random numbers
 curX = x0
-for i in xrange(1,10):
+for i in xrange(0,n):
 	curX = (a*curX + c) % m
-	print curX
-
+	numbers.append(curX)
 ##### ends generating random numbers
+
+print numbers
