8c6487547e72f46aa64db4d7ddb3dfad3b0f88c3
Lucas Vieira <lucasvg@poli.ufrj.br>
2017-10-13 18:21:09 -0300

    Uses native uniform function

--- exponential.py
+++ exponential.py
@@ -1,31 +1,15 @@
 import sys
 import matplotlib.pyplot as plt
 import math
+import numpy
 
-#### load parameters of Linear Congruent method
-# a of L. C. formula
-a = 37
-# M of L. C. formula
-m = 23597
-# c of L. C. formula
-c = 7
-# seed of L. C. formula
-x0 = 4
 # amount of random numbers to generate
 n = 100000
 # exponential rate
 rate = 1
 
-def genUniform(a, m, c, x0, n):
-	uniformDistrNumbers = []
-	curX = x0
-	for i in xrange(0,n):
-		curX = (a*curX + c) % m
-		uniformNumber = curX/float(m)
-		uniformDistrNumbers.append(uniformNumber)
-	return uniformDistrNumbers
+uDistr = numpy.random.uniform(low=0.0, high=1.0, size=n)
 
-uDistr = genUniform(a, m, c, x0, n)
 expDistrNumbers = []
 for u in uDistr:
 	expDistrNumbers.append( (-1/rate) * math.log(u) )
