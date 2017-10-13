531dcc8e68888c9f198a19f267541da3b38a0f6b
Lucas Vieira <lucasvg@poli.ufrj.br>
2017-10-13 19:42:17 -0300

    Creates normal with accept reject method

---
+++ normalARMethod.py
@@ -0,0 +1,33 @@
+import sys
+import matplotlib.pyplot as plt
+import math
+import numpy
+
+# amount of tries to generate
+n = 100000
+# exponential rate
+rate = 1
+
+def accept(u, y):
+	return u <= math.pow(math.e, -((y-1)**2)/2)
+
+# computes normal
+normalDistrNumbers = []
+for i in range(0, n):
+	uDistr = numpy.random.uniform(low=0.0, high=1.0, size=2)
+	uForExp = numpy.random.uniform(low=0.0, high=1.0, size=1)[0]
+	exp = ( (-1/rate) * math.log(uForExp) )
+
+	if(accept(uDistr[0], exp)):
+		if (uDistr[1] > 0.5):
+			normalDistrNumbers.append(exp)
+		else:
+			normalDistrNumbers.append(-exp)
+# ends computing normal
+
+data = normalDistrNumbers
+plt.hist(data, bins=50)
+plt.title("Histogram")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.show()
